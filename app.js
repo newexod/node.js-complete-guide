49f6deda43a3829326c5f671fa6ee51ce59cce1a
d1m0n <dpulenko@gbsfo.com>
2019-04-13 18:29:16 +0300

    Routing Requests

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  // process.exit(); // остановка сервера
+  if (req.url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');  
+    return res.end();
+  }
 
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
