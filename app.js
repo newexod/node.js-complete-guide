939edce8acb0d5b6890daa832b05adcce551efc4
d1m0n <dpulenko@gbsfo.com>
2019-04-16 13:50:15 +0300

    Installing & Implementing Pug

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views'); // 2-ой views название папки
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
