6cc9124486eaac541445526b1df1f7f4c86fb768
d1m0n <dpulenko@gbsfo.com>
2019-04-22 05:37:27 -0700

    Registering the Routes

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
-const productsControlles = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', productsControlles.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/ products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productsControlles.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
