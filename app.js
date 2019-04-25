63cf4ea36be1f37008372c5dacd796f60e421bc9
d1m0n <dpulenko@gbsfo.com>
2019-04-25 05:24:52 -0700

    Connecting App to the SQL Database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.set('views', 'views'); // 2-ой views название папки
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * from products');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
