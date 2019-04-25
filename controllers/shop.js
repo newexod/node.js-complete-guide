d4a35299183cc7ae85e8984616ccdd554e14d79e
d1m0n <dpulenko@gbsfo.com>
2019-04-25 05:54:03 -0700

    Fetching Products2

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,13 +2,17 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products, 
-      pageTitle: 'All Products', 
-      path: '/products'
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render('shop/product-list', {
+        prods: rows, 
+        pageTitle: 'All Products', 
+        path: '/products'
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  });
 };
 
 exports.getProduct = (req, res, next) => {
@@ -28,10 +32,10 @@ exports.getIndex = (req, res, next) => {
       console.log(rows);
       console.log(fieldData);
       res.render('shop/index', {
-      prods: rows, 
-      pageTitle: 'Shop', 
-      path: '/'
-    });
+        prods: rows, 
+        pageTitle: 'Shop', 
+        path: '/'
+      });
     })
     .catch(err => {
       console.log(err);
